289d4de496da7800c85bd9f587a117db3385d6cb
TomasAbalos99 <toomii.vdp@hotmail.com.ar>
2025-04-10 22:09:11 -0300

    Parte 2 terminada

--- TP1.js
+++ TP1.js
@@ -1,7 +1,8 @@
 const productos = [
     { id: 1, nombre: "Remera", precio: 1500, categoria: "Ropa" },
     { id: 2, nombre: "Pantalón", precio: 2500, categoria: "Ropa" },
-    { id: 3, nombre: "Zapatillas", precio: 8000, categoria: "Calzado" }, { id: 4, nombre: "Gorra", precio: 1200, categoria: "Accesorios" },
+    { id: 3, nombre: "Zapatillas", precio: 8000, categoria: "Calzado" }, {
+         id: 4, nombre: "Gorra", precio: 1200, categoria: "Accesorios" },
     { id: 5, nombre: "Campera", precio: 9500, categoria: "Ropa" }
     ];
 
@@ -19,3 +20,21 @@ const productos = [
     productos.forEach(producto => {
         console.log("producto:" + producto.nombre + ", Precio: " + producto.precio)
     });
+
+
+    //Parte 2
+    //4)
+    const nombresProductos = productos.map(p => p.nombre);
+    console.log(nombresProductos);
+
+    //5)
+    const soloRopa = productos.filter(p => p.categoria == "Ropa");
+    console.log(soloRopa);
+
+    //6)
+    const mayorA3000 = productos.filter(p => p.precio >= 3000);
+    console.log(mayorA3000);
+
+    //7)
+    const gorra = productos.find(p => p.nombre = "Gorra");
+    console.log(gorra)
